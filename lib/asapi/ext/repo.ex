8b21c154b94094943be7d8f124c0c30923784e12
tynn <tynn.dev@gmail.com>
2017-09-12 23:41:33 +0200

    Reorder supported repositories

--- lib/asapi/ext/repo.ex
+++ lib/asapi/ext/repo.ex
@@ -23,9 +23,9 @@ defmodule Asapi.Ext.Repo do
 
   @repos [
       "https://repo1.maven.org/maven2",
+      "https://maven.google.com",
       "https://jcenter.bintray.com",
-      "https://jitpack.io",
-      "https://maven.google.com"
+      "https://jitpack.io"
     ]
 
 
