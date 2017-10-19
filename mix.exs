642a4256925a6007a72567f6b04c2fac2f74f5b6
tynn <tynn.dev@gmail.com>
2017-10-19 02:40:09 +0200

    Update dependencies and Elixir version

--- mix.exs
+++ mix.exs
@@ -1,34 +1,32 @@
-defmodule Lv.Mixfile do
+defmodule Asapi.Mixfile do
   use Mix.Project
 
-  def project do
-    [
-      app: :asapi,
-      version: "0.1.0",
-      elixir: "~> 1.3",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
-      deps: deps(),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls.json": :test]
-    ]
-  end
+  def project, do: [
+    app: :asapi,
+    version: "0.2.0",
+    elixir: "~> 1.4",
+    build_embedded: Mix.env == :prod,
+    start_permanent: Mix.env == :prod,
+    deps: deps(),
+    test_coverage: [ tool: ExCoveralls ],
+    preferred_cli_env: [ "coveralls.json": :test ],
+    aliases: aliases()
+  ]
 
-  def application do
-    [
-      applications: [:cachex, :redix, :tesla, :trot],
-      mod: {Asapi.Ext, []}
-    ]
-  end
+  def application, do: [
+    mod: { Asapi.Ext, [] }
+  ]
 
-  defp deps do
-    [
-      {:trot, github: "hexedpackets/trot"},
-      {:tesla, "~> 0.7.1"},
-      {:redix, ">= 0.0.0"},
-      {:cachex, "~> 2.1"},
-      {:mock, "~> 0.2", only: :test},
-      {:excoveralls, "~> 0.7", only: :test}
-    ]
-  end
+  defp deps, do: [
+    { :trot, github: "hexedpackets/trot" },
+    { :tesla, "~> 0.7.1" },
+    { :redix, ">= 0.0.0" },
+    { :cachex, "~> 2.1" },
+    { :mock, "~> 0.2", only: :test },
+    { :excoveralls, "~> 0.7", only: :test }
+  ]
+
+  defp aliases, do: [
+    "run.all": [ "run --no-halt" ]
+  ]
 end
