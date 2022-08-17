5f5b3c605da2bd2caeb5f3a3f746b69f34b9e419
tynn <tynn.dev@gmail.com>
2022-08-17 20:56:32 +0200

    Upgrade Elixir and dependency versions

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Asapi.Mixfile do
 
   def project, do: [
     app: :asapi,
-    version: "0.4.0",
+    version: "0.5.0",
     elixir: "~> 1.11",
     build_embedded: Mix.env == :prod,
     start_permanent: Mix.env == :prod,
